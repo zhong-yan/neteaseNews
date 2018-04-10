d3a58804496eac304c2e4d67055c1ccf605d32d7
zhongyan <1763515750@qq.com>
2018-04-11 06:44:33 +0800

    爬虫部署完毕(news.py),运行正常

--- neteasenews/news.py
+++ neteasenews/news.py
@@ -3,8 +3,8 @@ from neteasenews.spider.coldspider import datablogspider, collegespider, govspid
 from neteasenews.spider.hotspider import hotspider, spider
 from neteasenews.spider.rankspider import rankspider
 # from neteasenews.spider.photospider import photospider
-from neteasenews.spider.config import JSON_INDEX_URLS
-from multiprocessing.pool import Pool
+import threading
+# from multiprocessing.pool import Pool
 
 
 # 考虑加入downloads()方法,分类下载到本地?
@@ -12,36 +12,88 @@ from multiprocessing.pool import Pool
 
 # 部署爬虫唯一出入口main()方法
 if __name__ == '__main__':
+    print('==============================================================\n')
     print('neteasenews Spider is working now, give me some patience,ok?\n')
-    print('爬取排行标签:')
-    rankspider()
-    time.sleep(5)
-    try:
-        print('爬取国内,国际,社会,军事,航空,无人机')
-        managerspider()
-    except None:
-        pass
-    print('开启冷更新,这些标签更新频率不大')
-    photospider()
-    list_spider = [indexspider, datablogspider, collegespider, govspider, gongyispider, mediaspider]
-    pools = Pool(7)
-    pools.map(newsSpider, list_spider)
-    print('============================================================\n')
-    keybords = int(input('是否再次运行爬虫?(1 means yes and 0 means no)\n'))
-    print('============================================================\n')
-    if keybords == 1:
-        print('neteasenews Spider is working now, give me some patience,ok?\n')
-        print('爬取排行标签:')
-        rankspider()
-        try:
-            print('爬取国内,国际,社会,军事,航空,无人机')
-            managerspider()
-        except None:
-            pass
-        print('开启冷更新,这些标签更新频率不大')
-        list_spider = [indexspider, photospider, datablogspider, collegespider, govspider, gongyispider, mediaspider]
-        pools = Pool(7)
-        pools.map(newsSpider, list_spider)
-    elif keybords == 0:
-        print('爬虫完毕!\t大吉大利\t今晚吃鸡')
-        print('********************************************************\n')
+    print('==============================================================\n')
+    print('1.爬虫全部运作\n')
+    print('2.热更新(只更新即时性新闻)\n')
+    print('3.冷更新(只更新变动频率低的新闻)\n')
+    print('4.下载图集(需要数据库中存储有适量数据)\n')
+    print('==============================================================\n')
+    choices = int(input('请输入你的选择:\n\t\t\t'))
+    print('--------------------------------------------------------------\n')
+    timeout = 0
+    if choices:
+        if choices == 1:
+            while True:
+                print('All of the Spider Will Be Running, Take It Easy!!!')
+                spider()
+                time.sleep(5)
+                rankspider()
+                time.sleep(5)
+                task_datablog = threading.Thread(target=datablogspider)
+                task_colleges = threading.Thread(target=collegespider)
+                task_gov = threading.Thread(target=govspider)
+                task_gongyi = threading.Thread(target=gongyispider)
+                task_media = threading.Thread(target=mediaspider)
+                tasks = [task_datablog, task_colleges, task_gov, task_gongyi, task_media]
+                for task in tasks:
+                    task.start()
+                for task_ in tasks:
+                    task_.join()
+                for task_run in tasks:
+                    if task_run.is_alive():
+                        print('Task is running now')
+                time.sleep(5)
+                timeout += 5
+                if timeout == 10:
+                    print('Spider will be off, wish no bugs or exceptions')
+                    print('大吉大利,今晚吃鸡')
+                    print('==============================================================\n')
+                    break
+        elif choices == 2:
+            while True:
+                hotspider()
+                rankspider()
+                # 理论上新闻更新速度根本没这么快,10S一篇新闻...666
+                print('please wait for 10s,it will run again!!')
+                time.sleep(10)
+                timeout += 10
+                # 一分钟结束,事实上并没有这么多更新快的新闻
+                if timeout == 20:
+                    print('热更新完毕')
+                    print('大吉大利,今晚吃鸡')
+                    print('==============================================================\n')
+                    break
+        elif choices == 3:
+            while True:
+                task_datablog = threading.Thread(target=datablogspider)
+                task_colleges = threading.Thread(target=collegespider)
+                task_gov = threading.Thread(target=govspider)
+                task_gongyi = threading.Thread(target=gongyispider)
+                task_media = threading.Thread(target=mediaspider)
+                tasks = [task_datablog, task_colleges, task_gov, task_gongyi, task_media]
+                for task in tasks:
+                    task.start()
+                for task_ in tasks:
+                    task_.join()
+                for task_run in tasks:
+                    if task_run.is_alive():
+                        print('Task is running now')
+                time.sleep(5)
+                timeout += 5
+                if timeout == 5:
+                    print('冷更新完毕')
+                    print('大吉大利,今晚吃鸡')
+                    print('==============================================================\n')
+                    break
+        elif choices == 4:
+            print('暂时未做处理!因为chromedriver速度太慢了')
+    time.sleep(10)
+    print('Spider will be off, wish no bugs or exceptions')
+    print('大吉大利,今晚吃鸡')
+    print('==============================================================\n')
+    # 展示部分数据
+
+
+
